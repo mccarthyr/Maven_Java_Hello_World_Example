c6bfd1be72d82d0ac36940688692e2cb239daff6
mccarthyr <mccarthy.richard@gmail.com>
2016-06-28 21:34:41 +0100

    updated the output message

--- src/main/java/com/fireduptech/build/maven/java/App.java
+++ src/main/java/com/fireduptech/build/maven/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello the world of Java Maven!" );
+        System.out.println( "Hello World, Java and Maven!" );
     }
 }
