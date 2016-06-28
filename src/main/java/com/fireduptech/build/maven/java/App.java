8cc6e128a79f513fcb0cccab5a1b056db4d3669e
mccarthyr <mccarthy.richard@gmail.com>
2016-06-28 16:57:07 +0100

    Importing the Maven Java Hello World Demonstration

---
+++ src/main/java/com/fireduptech/build/maven/java/App.java
@@ -0,0 +1,13 @@
+package com.fireduptech.build.maven.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello the world of Java Maven!" );
+    }
+}
